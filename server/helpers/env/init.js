6e0ad4dfe8f4b630b610958f7f2e8715d9b32bd4
Daniel Reszka <dannn.r@gmail.com>
2019-01-26 22:38:52 +0100

    also load env vars from .env.local in server

--- server/helpers/env/init.js
+++ server/helpers/env/init.js
@@ -2,6 +2,13 @@ const dotenv = require('dotenv')
 const path = require('path')
 
 const env = process.env.NODE_ENV
+
+;(() => {
+  const file = '.env.local'
+  console.log(`[server] Loading local env vars "${env}" from: "${file}"`)
+  dotenv.load({ path: path.resolve(file) })
+})()
+
 if (env === 'dev' || env === 'development') {
   const file = '.env'
   console.log(`[server] Loading environment "${env}" from: "${file}"`)
