0000b98b1c33a38ef9c2f34c3ebbc25df39915d4
Daniel Reszka <dannn.r@gmail.com>
2019-01-26 10:32:58 +0100

    improve .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,53 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true,
-        "jest": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
+  "extends": [
+    "prettier",
+    "prettier/react",
+    "react-app"
+  ],
+  "plugins": [
+    "prettier",
+    "react",
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+      "experimentalObjectRestSpread": true,
+      "modules": true,
     }
-};
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+  },
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+        "trailingComma": "es5",
+        "semi": false,
+        "printWidth": 80,
+      },
+    ],
+    "no-unused-vars": ["error", { "args": "after-used" }],
+    "react/jsx-uses-vars": [2],
+    "react/jsx-uses-react": [2],
+    "react/react-in-jsx-scope": [2],
+  },
+  "overrides": [
+    {
+      "files": ["*.test.js"],
+       "rules": {
+          "no-unused-expressions": "off"
+      }
+    }
+  ],
+  "settings": {
+    "react": {
+      "version": "detect",
+    },
+  }
+}
