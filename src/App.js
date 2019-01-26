bb05cdab535ef4086e88504b097accf891029572
Daniel Reszka <dannn.r@gmail.com>
2019-01-26 02:27:35 +0100

    fetch and render products from api

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
+  state =  { products: []}
+
+  componentDidMount = async () => {
+    const { products } = await (await fetch('/api/products')).json()
+    this.setState({ products })
+  }
+
   render() {
     return (
-      <div>
-        Hello
+      <div className="container">
+        <h1>Products</h1>
+
+        {this.state.products.map(p =>
+          <>
+            <div>
+              <h2>
+                {p.title}
+              </h2>
+
+              <div>
+                {p.description}
+              </div>
+            </div>
+            <hr />
+          </>
+        )}
       </div>
     )
   }
