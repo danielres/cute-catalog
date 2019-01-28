dca6b227f21930355cdb32b9629d9d9fe3c3754b
Daniel Reszka <dannn.r@gmail.com>
2019-01-28 11:52:27 +0100

    remove unneeded skipUndefined in route products

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,9 +4,7 @@ const router = express.Router()
 const Product = require('../models/Product')
 
 router.get('/', async (req, res) => {
-  const products = await Product.query()
-    .skipUndefined()
-    .orderBy('title')
+  const products = await Product.query().orderBy('title')
 
   res.json({ products })
 })
