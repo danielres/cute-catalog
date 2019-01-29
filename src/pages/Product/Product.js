566fafdcf7c1d856434addea1aa7c39d1754654c
Daniel Reszka <dannn.r@gmail.com>
2019-01-29 15:07:48 +0100

    add prev/next links to Product

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,11 +1,34 @@
+import Link from 'redux-first-router-link'
 import React from 'react'
+
+import { toProduct } from 'store/routerActions'
 
 import Loader from 'shared/Loader'
 import ProductCard from 'shared/ProductCard'
 
-const Product = ({ isLoading, product }) => (
+const Product = ({ isLoading, product, nextProduct, prevProduct }) => (
   <Loader isLoading={isLoading}>
-    <h2>{product.title}</h2>
+    <div className="row">
+      <div className="col-1">
+        <Link
+          className="link-gray"
+          to={toProduct({ productId: prevProduct.id })}
+        >
+          prev
+        </Link>
+      </div>
+
+      <h2 className="col text-center">{product.title}</h2>
+
+      <div className="col-1">
+        <Link
+          className="link-gray"
+          to={toProduct({ productId: nextProduct.id })}
+        >
+          next
+        </Link>
+      </div>
+    </div>
     <ProductCard product={product} />
   </Loader>
 )
--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,14 +1,26 @@
 import { connect } from 'react-redux'
 
+import getNextItem from './getNextItem'
+import getPrevItem from './getPrevItem'
+
 import Product from './Product'
+
 const emptyObject = {}
 
 const mapStateToProps = ({ location, products }) => {
   const { productId } = location.payload
   const product = products.items[productId] || emptyObject
 
+  const nextProduct =
+    getNextItem({ items: products.items, currId: product.id }) || emptyObject
+
+  const prevProduct =
+    getPrevItem({ items: products.items, currId: product.id }) || emptyObject
+
   return {
     isLoading: !product.title,
+    nextProduct,
+    prevProduct,
     product,
   }
 }
