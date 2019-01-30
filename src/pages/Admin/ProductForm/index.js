7765cb8bf7eae00a3f46514160fa29635cbf86f8
Daniel Reszka <dannn.r@gmail.com>
2019-01-30 19:55:17 +0100

    enable ProductForm for new product

--- src/pages/Admin/ProductForm/index.js
+++ src/pages/Admin/ProductForm/index.js
@@ -3,13 +3,18 @@ import { connect } from 'react-redux'
 import ProductForm from './ProductForm'
 
 const emptyObject = {}
+const newProduct = { title: '', description: '', imageSrc: '' }
 
 const mapStateToProps = ({
   products: { items: products },
   location: { query: { product: productId } = emptyObject } = emptyObject,
-}) => ({
-  product: products[productId],
-})
+}) => {
+  const isNew = productId === 'new'
+
+  return {
+    product: isNew ? newProduct : products[productId],
+  }
+}
 
 const mapDispatchtoProps = () => ({
   onSubmit: values => console.log({ values }),
