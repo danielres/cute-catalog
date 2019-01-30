9669f3eeebe1be472c5e93fedd51ba5ad3c18a2f
Daniel Reszka <dannn.r@gmail.com>
2019-01-30 11:55:08 +0100

    improve/fix products cards layout

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -5,24 +5,19 @@ import { toProduct } from 'store/routerActions'
 
 import ProductCard from 'shared/ProductCard'
 
-const Product = ({ product }) => (
-  <div className="card-deck col-md-4">
-    <Link
-      className="d-flex link-gray"
-      to={toProduct({ productId: product.id })}
-    >
-      <ProductCard product={product} />
-    </Link>
-  </div>
-)
-
 export default ({ products }) => (
   <>
     <h2 className="invisible">Products</h2>
 
-    <div className="row">
+    <div className="card-deck no-gutters">
       {products.map(product => (
-        <Product key={product.id} product={product} />
+        <Link
+          className="d-flex col-sm-6 col-md-4 col-xl-3 link-gray"
+          key={product.id}
+          to={toProduct({ productId: product.id })}
+        >
+          <ProductCard product={product} />
+        </Link>
       ))}
     </div>
   </>
