886e228f264fc38ffe4bfb8c27740881fc2ee214
Daniel Reszka <dannn.r@gmail.com>
2019-02-08 13:01:48 +0100

    add endpoint /api/admin/images/uploads/endpoint

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+
+const getImageUploadEndpointAndSignature = require('../../helpers/imagekit/getImageUploadEndpointAndSignature')
+
 const router = express.Router()
 
 const Product = require('../../models/Product')
@@ -21,6 +24,22 @@ router.put('/products/:id', async (req, res, next) => {
     res.json({ success: true })
   } catch (e) {
     next(ServerError(422, 'Could not update product', e))
+  }
+})
+
+router.get('/images/uploads/endpoint', (req, res, next) => {
+  try {
+    const { filename } = req.query
+    const folder = '/cute-catalog'
+    res.json(getImageUploadEndpointAndSignature({ filename, folder }))
+  } catch (e) {
+    next(
+      ServerError(
+        500,
+        'Could not return image upload endpoint and signature',
+        e
+      )
+    )
   }
 })
 
