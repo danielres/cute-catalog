49bca824fc33ba08e5750792fd4d2ae54bc432d2
Daniel Reszka <dannn.r@gmail.com>
2019-02-08 13:27:37 +0100

    add support for error modals + fix classNames in Modals

--- src/Modals/index.js
+++ src/Modals/index.js
@@ -6,35 +6,44 @@ import Help from 'Modals/contents/Help'
 import UploadImage from 'Modals/contents/UploadImage'
 
 const Header = ({ children }) => (
-  <div class="modal-header">
-    <h4 class="modal-title">{children}</h4>
-    <button class="close" onClick={closeModal} style={{ outline: 'none' }}>
+  <div className="modal-header">
+    <h4 className="modal-title">{children}</h4>
+    <button className="close" onClick={closeModal} style={{ outline: 'none' }}>
       &times;
     </button>
   </div>
 )
 
 const Modal = ({ children, title }) => (
-  <div class="modal d-block">
-    <div class="modal-dialog">
-      <div class="modal-content">
+  <div className="modal d-block">
+    <div className="modal-dialog">
+      <div className="modal-content">
         <Header>{title}</Header>
-        <div class="modal-body">{children}</div>
+        <div className="modal-body">{children}</div>
       </div>
     </div>
   </div>
 )
 
-const Backdrop = () => <div class="modal-backdrop" style={{ opacity: 0.5 }} />
+const getErrorMessage = payload => {
+  if (payload && payload.code && payload.code === 401)
+    return 'Unauthorized action, please login first.'
+  if (payload && payload.message) return payload.message
+  return 'Something went wrong, please try again'
+}
+
+const Backdrop = () => (
+  <div className="modal-backdrop" style={{ opacity: 0.5 }} />
+)
 
 class ModalContainer extends Component {
   constructor(props) {
     super(props)
-    this.state = { queue: [] }
+    this.state = { queue: ['UPLOAD_IMAGE'] }
     const getQueue = () => this.state.queue
 
-    bus.take('OPEN_MODAL', ({ name }) => {
-      this.setState({ queue: [name, ...getQueue()] })
+    bus.take('OPEN_MODAL', ({ name, payload }) => {
+      this.setState({ queue: [{ name, payload }, ...getQueue()] })
     })
 
     bus.take('CLOSE_MODAL', () =>
@@ -45,23 +54,27 @@ class ModalContainer extends Component {
   }
 
   render() {
-    const modal = this.state.queue[0]
+    const { name, payload } = this.state.queue[0]
 
     return (
       <>
-        {modal === 'UPLOAD_IMAGE' && (
+        {name === 'ERROR' && (
+          <Modal title="Error">{getErrorMessage(payload)} </Modal>
+        )}
+
+        {name === 'UPLOAD_IMAGE' && (
           <Modal title="Upload Image">
             <UploadImage />
           </Modal>
         )}
 
-        {modal === 'VIEW_HELP' && (
+        {name === 'VIEW_HELP' && (
           <Modal title="Help">
             <Help />
           </Modal>
         )}
 
-        {modal && <Backdrop />}
+        {name && <Backdrop />}
       </>
     )
   }
