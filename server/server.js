a2e7afa1a3f31ce134c104e1e8b0c07372ba3b47
Daniel Reszka <dannn.r@gmail.com>
2019-02-11 21:46:46 +0100

    enble helmet with defaults in server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./db/init')
 
 const cookieParser = require('cookie-parser')
 const express = require('express')
+const helmet = require('helmet')
 const logger = require('morgan')
 const path = require('path')
 
@@ -10,12 +11,12 @@ const ensureAdmin = require('./middleware/ensureAdmin')
 const verifyToken = require('./middleware/verifyToken')
 
 const app = express()
-app.use(express.static(path.join(__dirname, '..', 'build/')))
-
+app.use(cookieParser())
+app.use(helmet())
 app.use(logger('dev'))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'build/')))
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 
 app.use('/api/login', require('./routes/login'))
 app.use('/api/logout', require('./routes/logout'))
