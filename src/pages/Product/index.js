83a5b00e0dc181870d23613000fe4c2d4bd830e1
Daniel Reszka <dannn.r@gmail.com>
2019-02-13 21:18:25 +0100

    fix prev/next thumbnails in wrong order since order changed to desc

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -11,11 +11,14 @@ const mapStateToProps = ({ location, products }) => {
   const { productId } = location.payload
   const product = products.items[productId] || emptyObject
 
-  const nextProduct =
+  const reversed = true
+  const nextItem =
     getNextItem({ items: products.items, currId: product.id }) || emptyObject
-
-  const prevProduct =
+  const prevItem =
     getPrevItem({ items: products.items, currId: product.id }) || emptyObject
+
+  const nextProduct = reversed ? prevItem : nextItem
+  const prevProduct = reversed ? nextItem : prevItem
 
   return {
     isLoading: !product.title,
