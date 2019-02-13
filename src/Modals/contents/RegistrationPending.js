bfef28024758be6b01512a3758093b3b65eb85d4
Daniel Reszka <dannn.r@gmail.com>
2019-02-13 12:42:21 +0100

    removed unneeded Login link in modal RegistrationPending

--- src/Modals/contents/RegistrationPending.js
+++ src/Modals/contents/RegistrationPending.js
@@ -1,8 +1,4 @@
-import Link from 'redux-first-router-link'
 import React from 'react'
-
-import { closeModal } from 'Modals/bus'
-import { toLogin } from 'store/routerActions'
 
 export default () => (
   <>
@@ -10,11 +6,6 @@ export default () => (
     <p>
       Please check your mailbox and use the provided confirmation link to
       activate your account.
-    </p>
-    <p>
-      <Link onClick={closeModal} to={toLogin()}>
-        Login
-      </Link>
     </p>
   </>
 )
